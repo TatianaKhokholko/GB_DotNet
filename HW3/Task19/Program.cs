fc461271b5e9b8134e62264c110dedfa486e800e
Tatiana <tatianaBelkaRZN@gmail.com>
2022-10-10 19:33:54 +0300

    HW3

---
+++ HW3/Task19/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+public static class Programm
+{
+    public static void Main()
+    {
+        Console.WriteLine("Input five-digit number and press enter");
+        string? number = Console.ReadLine();
+        CheckLengthAndPrintResult(number);
+    }
+
+    private static void CheckLengthAndPrintResult(string number)
+    {
+        if (number?.Length == 5)
+        {
+            if (number[0] == number[4] && number[1] == number[3])
+            {
+                Console.WriteLine($"{number} is palindrome");
+            }
+            else Console.WriteLine($"{number} is not palindrome");
+        }
+        else
+        {
+            Console.WriteLine($"The number isn't five-digit!");
+        }
+    }
+}
---
+++ HW3/Task23/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+ 3 -> 1, 8, 27
+ 5 -> 1, 8, 27, 64, 125
+*/
+
+public static class Programm
+{
+    public static void Main()
+    {
+        Console.WriteLine("Input integer number");
+        int number = int.Parse(Console.ReadLine());
+        countPowAndPrintResult(number);
+    }
+
+    private static void countPowAndPrintResult(int inputValue)
+    {
+        Console.Write($"{inputValue} -> ");
+
+        if (inputValue != 0)
+        {
+            for (int i = 1; i <= inputValue; i++)
+            {
+                Console.Write($"{Math.Pow(i, 3)}, ");
+            }
+        }
+        else
+        {
+            Console.WriteLine("0 * 0 = 0\n=)");
+        }
+    }
+}
