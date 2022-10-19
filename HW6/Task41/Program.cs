99482014ffabd555c912aa520a0564a3f1b89b58
Tatiana <tatianaBelkaRZN@gmail.com>
2022-10-19 17:46:41 +0300

    HW6

---
+++ HW6/Task41/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+public static class Program
+{
+    public static void Main()
+    {
+        int[] arrayNumbers = Terms("Input some numbers\t");
+        CountNumbersBiggerZero(arrayNumbers);
+    }
+
+    private static int[] Terms(string message)
+    {
+        Console.Write(message);
+        string customnumber = Console.ReadLine();
+        int[] numbers = customnumber.Split(' ', ',', ';').Select(int.Parse).ToArray();
+        return numbers;
+    }
+
+    private static void CountNumbersBiggerZero(int[] array)
+    {
+        int countNumberBiggerZero = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 0)
+            {
+                countNumberBiggerZero++;
+            }
+            else { }
+        }
+        Console.Write($"Count = {countNumberBiggerZero}");
+    }
+}
---
+++ HW6/Task43/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+------
+x=(b2-b1)/(k1-k2)
+------
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+public static class Program
+{
+    public static void Main()
+    {
+        double b1 = Terms("Input value for b1\t");
+        double b2 = Terms("Input value for b2\t");
+
+        double k1 = Terms("Input value for k1\t");
+        double k2 = Terms("Input value for k2\t");
+
+        double x = FindXValue(b1, k1, b2, k2);
+        FindsPointIntersectionOfTwoLines(b1, k1, b2, k2, x);
+
+    }
+
+    private static double Terms(string message)
+    {
+        Console.Write(message);
+        double number = double.Parse(Console.ReadLine());
+        return number;
+    }
+
+    private static double FindXValue(double b1, double k1, double b2, double k2)
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        Console.WriteLine($"x = {x}");
+        return x;
+    }
+
+    private static void FindsPointIntersectionOfTwoLines(double b1, double k1, double b2, double k2, double x)
+    {
+        double y1 = k1 * x + b1;
+        double y2 = k2 * x + b2;
+        Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({y1}; {y2})");
+    }
+}
