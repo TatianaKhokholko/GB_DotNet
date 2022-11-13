29668973f24cf7d1ec14c37d7609a8d86d2c206f
Tatiana <tatianaBelkaRZN@gmail.com>
2022-11-13 18:10:46 +0300

    HW8 first part

---
+++ HW8/Task54/Program.cs
@@ -0,0 +1,60 @@
+﻿/* 
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+public static class Program
+{
+    public static void Main()
+    {
+        int[,] finalArray = {
+             { 1, 4, 7, 2},
+             { 5, 9, 2, 3 },
+             { 8, 4, 2, 4 }
+            };
+        PrintArray(finalArray);
+        SortInDescendingOrder(finalArray);
+    }
+
+    private static void PrintArray(int[,] array)
+    {
+        for (int row = 0; row < array.GetLength(0); row++)
+        {
+            for (int column = 0; column < array.GetLength(1); column++)
+            {
+                Console.Write(array[row, column] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    private static void SortInDescendingOrder(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                for (int fordesc = 0; fordesc < array.GetLength(1) - 1; fordesc++)
+                {
+                    if (array[i, fordesc] < array[i, fordesc + 1])
+                    {
+                        int descArrayResult = array[i, fordesc + 1];
+                        array[i, fordesc + 1] = array[i, fordesc];
+                        descArrayResult = array[i, fordesc];
+                    }
+                }
+            }
+        }
+        Console.WriteLine();
+        PrintArray(array);
+    }
+}
---
+++ HW8/Task56/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+public static class Program
+{
+    public static void Main()
+    {
+        int[,] finalArray = {
+             { 1, 4, 7, 2},
+             { 5, 9, 2, 3},
+             { 8, 4, 2, 4},
+             { 5, 2, 6, 7}
+            };
+        Console.WriteLine("Default array:");
+        PrintArray(finalArray);
+        CountSumForLinesAndPrint(finalArray);
+
+    }
+
+    private static void PrintArray(int[,] array)
+    {
+        for (int row = 0; row < array.GetLength(0); row++)
+        {
+            for (int column = 0; column < array.GetLength(1); column++)
+            {
+                Console.Write(array[row, column] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    private static void CountSumForLinesAndPrint(int[,] array)
+    {
+        int minSum = 0;
+        int sumForLine = array[0, 0];
+        for (int i = 1; i < array.GetLength(1); i++)
+        {
+            sumForLine += array[0, i];
+        }
+
+        for (int j = 1; j < array.GetLength(0); j++)
+        {
+            int temp = array[1, 0];
+            for (int k = 1; k < array.GetLength(1); k++)
+            {
+                temp += array[k, k];
+            }
+            if (sumForLine > temp)
+            {
+                sumForLine = temp;
+                minSum = j;
+            }
+        }
+        Console.WriteLine($"\n{minSum + 1} - the line with min sum = {sumForLine}\n");
+    }
+}
